6106f22a8889c5a3f83d2255599d5385a3b68059
ErnieBuyco <buycoernie@gmail.com>
2020-02-13 20:36:11 +0800

    addedvoid

--- components/PokedexSearch.tsx
+++ components/PokedexSearch.tsx
@@ -1,7 +1,9 @@
 import React, { Component, RefObject, createRef} from 'react'
 import User from './User.interface';
-import SearchState from './Search.interface',
+import SearchState from './Search.interface';
 import PokeStyle from './styles/PokeStyle';
+
+
 
 export class PokedexSearch extends Component<User, SearchState> {
     pokemonRef: RefObject<HTMLInputElement>;
@@ -9,14 +11,11 @@ export class PokedexSearch extends Component<User, SearchState> {
         super(props);
         this.state = {
             error: false,
-            name: '',
-            numberofAbilites: 0,
-            baseExperience: 0,
-            imageUrl: ''
+           pokemon: null
         }
         this.pokemonRef = createRef();
     }
-    onSearchClick= () => {
+    onSearchClick= (): void => {
             const inputValue = this.pokemonRef.current.value;
             fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
             .then(res => {
@@ -27,32 +26,36 @@ export class PokedexSearch extends Component<User, SearchState> {
                     res.json().then(data => {
                            this.setState ({
                                 error: false,
-                                name: data.name,
-                                numberofAbilites: data.abilities.length,
-                                baseExperience: data.base_experience,
-                                imageUrl: data.sprites.front_default
+                                pokemon: {
+                                    name: data.name,
+                                    numberofAbilites: data.abilities.length,
+                                    baseExperience: data.base_experience,
+                                    imageUrl: data.sprites.front_default
+                                }
                            })
                     })
             })
     }
-
+    
     render() {
         const { name: userName, numberofPokemons} = this.props;
-        const { error, name, numberofAbilites, baseExperience, imageUrl} = this.state;
+        const { error, 
+                pokemon
+        } = this.state;
         
         let resultMarkup;
 
         if(error){
             resultMarkup = <p>Pokemon not found please try again</p>
-        } else {
+        } else if(this.state.pokemon){
             resultMarkup = 
             <div>
-                    <img src={imageUrl} 
+                    <img src={pokemon.imageUrl} 
                     alt="pokemon" 
                     className="pokemon__image__style"
                     />
                     <p>
-                        {name} has {numberofAbilites} and {baseExperience} base experience points.
+                        {name} has {pokemon.numberofAbilites} and {pokemon.baseExperience} base experience points.
                     </p>
             </div>
         }
--- components/Search.interface.tsx
+++ components/Search.interface.tsx
@@ -1,6 +1,10 @@
 
 export default interface Search{
     error: boolean,
+    pokemon: Pokemon
+}
+
+interface Pokemon{
     name: string,
     numberofAbilites: number,
     baseExperience: number,
